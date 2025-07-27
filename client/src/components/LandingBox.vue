0d4a99476cc3764990348e0452ea4e3cabd57b6a
DiogoFabricioAG <diogo.abregu.g@uni.pe>
2025-07-27 18:29:07 -0500

    landing created, but not finished, 'cause im still lazy af

---
+++ client/src/components/LandingBox.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+defineProps<{
+  msg: string
+  imgSrc?: string
+}>()
+</script>
+
+<template>
+  <div
+    class="border-2 border-yellow-300 shadow-lg shadow-white/20 p-2 cursor-pointer hover:rotate-2 hover:scale-105 duration-150"
+  >
+    <div class="border border-gray-300 pb-2">
+      <img
+        v-if="imgSrc === 'seed'"
+        class="size-[300px] w-full object-cover"
+        src="@/assets/seed-pros.png"
+        alt="Black and white Img"
+      />
+      <img
+        v-else-if="imgSrc === 'brain'"
+        class="size-[300px] w-full object-cover"
+        src="@/assets/brain-pros.png"
+        alt="Black and white Img"
+      />
+      <img
+        v-else-if="imgSrc === 'tree'"
+        class="size-[300px] w-full object-cover"
+        src="@/assets/tree-pros.png"
+        alt="Black and white Img"
+      />
+      <h1 class="text-2xl font-bold">{{ msg }}</h1>
+    </div>
+  </div>
+</template>
---
+++ client/src/views/LandingView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import LandingBox from '@/components/LandingBox.vue'
+</script>
+
+<template>
+  <main class="mt-16 w-4/5 mx-auto text-center">
+    <div name="landing-content" class="flex flex-col gap-4 items-center">
+      <h2 class="text-7xl">Bienvenido a <span class="text-yellow-300 font-bold">Forman</span></h2>
+      <div>
+        <p class="text-xl">La plataforma que rediseña tus textos</p>
+        <p class="text-xl">Y los transforma en tus ideas</p>
+      </div>
+    </div>
+    <div class="grid grid-cols-3 gap-6 mt-8">
+      <LandingBox msg="Empieza a crear" img-src="seed" />
+      <LandingBox msg="Rediseña tus ideas" img-src="brain" />
+      <LandingBox msg="Resultados sorprendentes" img-src="tree" />
+    </div>
+  </main>
+</template>
